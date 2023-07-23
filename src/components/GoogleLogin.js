13e11bf2e3de710cd2157f4914ee87685029285d
Michael Dawson <m.dawson@y7mail.com>
2023-07-23 11:52:32 -0500

    Initial commit - create react app, firebase setup

---
+++ src/components/GoogleLogin.js
@@ -0,0 +1,76 @@
+import { gapi, loadAuth2 } from 'gapi-script';
+import React, { useEffect, useState } from 'react';
+
+import './GoogleLogin.css';
+import { UserCard } from './UserCard';
+
+export const GoogleLogin  = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const setAuth2 = async () => {
+      const auth2 = await loadAuth2(gapi, process.env.REACT_APP_CLIENT_ID, process.env.REACT_APP_SCOPES)
+      if (auth2.isSignedIn.get()) {
+          updateUser(auth2.currentUser.get())
+      } else {
+          attachSignin(document.getElementById('customBtn'), auth2);
+      }
+    }
+    setAuth2();
+  }, []);
+
+  useEffect(() => {
+    if (!user) {
+      const setAuth2 = async () => {
+        const auth2 = await loadAuth2(gapi, process.env.REACT_APP_CLIENT_ID, process.env.REACT_APP_SCOPES)
+        attachSignin(document.getElementById('customBtn'), auth2);
+      }
+      setAuth2();
+    }
+  }, [user])
+
+  const updateUser = (currentUser) => {
+    const name = currentUser.getBasicProfile().getName();
+    const profileImg = currentUser.getBasicProfile().getImageUrl();
+    setUser({
+      name: name,
+      profileImg: profileImg,
+    });
+  };
+
+  const attachSignin = (element, auth2) => {
+    auth2.attachClickHandler(element, {},
+      (googleUser) => {
+        updateUser(googleUser);
+      }, (error) => {
+      console.log(JSON.stringify(error))
+    });
+  };
+
+  const signOut = () => {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      setUser(null);
+      console.log('User signed out.');
+    });
+  }
+
+  if(user) {
+    return (
+      <div className="container">
+        <UserCard user={user} />
+        <div id="" className="btn logout" onClick={signOut}>
+          Logout
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div id="customBtn" className="btn login">
+        Login
+      </div>
+    </div>
+  );
+}
